abeedb4b7e9fbff50ab2942b32ab178ba21c9dd1
Wesley <wesley100002001@gmail.com>
2016-01-26 14:19:32 +0800

    Add datepicker to activity page

--- src/app/features/activity/activity.controller.js
+++ src/app/features/activity/activity.controller.js
@@ -3,15 +3,39 @@ export default class ActController {
     var scope = this;
     scope.cardCover = require('../../../assets/imgs/default-activity-cover.jpg');
     scope.infoCover = require('../../../assets/imgs/mock-700x300.jpg');
+    
     if (!acl.checkStatus($cookies.get('status'))) {
       $state.go('login');
     }
+
+    // Datepicker options
+    scope.startOpened = false;
+    scope.endOpened = false;
+    scope.timeOption = {
+        max: new Date()
+    };
+    scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+    scope.altInputFormats = ['M!/d!/yyyy'];
+
     $http({
       method: 'GET',
       url: 'http://localhost:5000/dashboard/api/v1/activities/' + $stateParams.actId
-    }).then(function success(response) {
+    }).then(function (response) {
       scope.info = response.data.activity;
+      scope.info.start_time = new Date(scope.info.start_time);
+      scope.info.end_time = new Date(scope.info.end_time);
     }, function (response) {});
+  }
+
+  startOpen () {
+    this.startOpened = !this.startOpened;
+  }
+
+  endOpen () {
+    this.endOpened = !this.endOpened;
   }
 }
 
--- src/app/features/activity/index.js
+++ src/app/features/activity/index.js
@@ -2,6 +2,7 @@ import './activity.less';
 
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+import uibootstrap from 'angular-ui-bootstrap';
 
 import routing from './activity.routes';
 import FooterController from '../../../components/footer/footer.controller';
@@ -9,7 +10,7 @@ import ActController from './activity.controller';
 import NavbarController from '../../../components/navbar/navbar.controller';
 import acl from '../../services/acl.service';
 
-export default angular.module('app.activity', [uirouter, acl])
+export default angular.module('app.activity', [acl, uibootstrap, uirouter])
   .config(routing)
   .controller('ActController', ActController)
   .controller('NavbarController', NavbarController)
