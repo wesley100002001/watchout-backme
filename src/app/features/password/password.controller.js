9cf645ef32decf70baef968ad3bcbc349f422e4a
Wesley <wesley100002001@gmail.com>
2016-07-09 11:37:15 +0800

    [Password] Implement chagnge password page

--- src/app/features/password/password.controller.js
+++ src/app/features/password/password.controller.js
@@ -1,17 +1,37 @@
 export default class PwdController {
-  constructor ($state, $cookies, acl, $http, restful) {
+  constructor ($state, $cookies, acl, $http, restful, $q) {
     var scope = this;
     scope.state = $state;
     scope.cookies = $cookies;
+    this.restful = restful;
+    this.$q = $q;
 
     if (!acl.checkStatus(this.cookies.get('status'))) {
       this.state.go('login');
     }
+  }
 
-    restful.getAttrs().then(function (response) {
-      scope.list = response.data.attractions;
-    }, function (response) {});
+  changePwd () {
+    if (!!this.oldpwd && !!this.newpwd) {
+      this.restful.getAdminPwd()
+      .then(response => {
+        if (response === this.oldpwd) {
+          return this.restful.setAdminPwd(this.newpwd)
+        } else {
+          alert('舊密碼錯誤，請重新輸入');
+          return this.$q(function (resolve, reject) {
+            resolve('Error');
+          });
+        }
+      }).then(response => {
+        if (response !== 'Error') {
+          alert('新密碼設定完成');
+        }
+      });
+    } else {
+      alert('舊密碼及新密碼不可為空白');
+    }
   }
 }
 
-PwdController.$inject = ['$state', '$cookies', 'acl', '$http', 'restful'];
+PwdController.$inject = ['$state', '$cookies', 'acl', '$http', 'restful', '$q'];
--- src/app/services/restful.service.js
+++ src/app/services/restful.service.js
@@ -5,36 +5,13 @@ class Restful {
   constructor ($http, $q) {
     this.http = $http;
     this.$q = $q;
-    this.api = 'http://pilot-hi-1.4free.com.tw/api/';
-    this.mockApi = 'http://localhost:5000/dashboard/api/v1/';
     var config = {
-      apiKey: "AIzaSyCbvS9EsfPK53S6LJrlI0zy3ec0qV3SgMQ",
-      authDomain: "watchout-backme.firebaseapp.com",
-      databaseURL: "https://watchout-backme.firebaseio.com",
-      storageBucket: "watchout-backme.appspot.com",
+      apiKey: 'AIzaSyCbvS9EsfPK53S6LJrlI0zy3ec0qV3SgMQ',
+      authDomain: 'watchout-backme.firebaseapp.com',
+      databaseURL: 'https://watchout-backme.firebaseio.com',
+      storageBucket: 'watchout-backme.appspot.com',
     };
     firebase.initializeApp(config);
-  }
-
-  getTest () {
-    var userId = '12345';
-    var name = 'Wesley';
-    var email = 'woeifjwoeifjowi@gmail';
-    firebase.database().ref('admin/').set({
-      account: 'admin',
-      password: '1234'
-    }).then(function () {
-      alert('Success');
-    });
-    // ref.child('first').set('Ada').then(function () {
-    //   alert('Success');
-    // });
-    // ref.child('last').set('Lovelace');
-    // alert('kkk');
-    firebase.database().ref('admin/').on('value', function(snapshot) {
-      alert(JSON.stringify(snapshot.val()));
-    });
-    // alert('jijoij');
   }
 
   getAdmin (account, pwd) {
@@ -42,6 +19,25 @@ class Restful {
       firebase.database().ref('admin/').on('value', function (snapshot) {
         resolve(snapshot.val().account === account && snapshot.val().password === pwd);
       });
+    });
+  }
+
+  getAdminPwd () {
+    return this.$q(function (resolve, reject) {
+      firebase.database().ref('admin/').on('value', function (snapshot) {
+        resolve(snapshot.val().password);
+      });
+    });
+  }
+
+  setAdminPwd (pwd) {
+    return this.$q(function (resolve, reject) {
+      firebase.database().ref('admin/').set({
+        account: 'admin',
+        password: pwd
+      }).then(function () {
+        resolve('Success');
+      })
     });
   }
 
@@ -68,27 +64,6 @@ class Restful {
         resolve(snapshot.val());
       });
     })
-  }
-
-  getSites () {
-    return this.http({
-      method: 'GET',
-      url: this.api + 'sites/'
-    });
-  }
-
-  getActs () {
-    return this.http({
-      method: 'GET',
-      url: this.mockApi + 'activities/'
-    });
-  }
-
-  getAttrs () {
-    return this.http({
-      method: 'GET',
-      url: this.mockApi + 'attractions/'
-    });
   }
 
   getLastUpdateTime () {
