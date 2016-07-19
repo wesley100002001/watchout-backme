e2f2288fbadc04ca4aa40cee1d89dd9fa447912e
Wesley <wesley100002001@gmail.com>
2016-07-19 17:34:30 +0800

    Add filter buttons for orders page

--- src/app/features/orders/orders.controller.js
+++ src/app/features/orders/orders.controller.js
@@ -1,11 +1,14 @@
 import moment from 'moment';
 
 export default class OrdersController {
-  constructor ($state, $cookies, acl, $http, restful, $filter, $scope, $q) {
+  constructor ($state, $cookies, acl, $http, restful, $filter, $scope, $q,
+    $rootScope) {
     var scope = this;
     this.restful = restful;
     this.state = $state;
     this.cookies = $cookies;
+    this.rs = $rootScope;
+    this.searchObj = {};
 
     if (!acl.checkStatus(this.cookies.get('status'))) {
       this.state.go('login');
@@ -22,7 +25,7 @@ export default class OrdersController {
     this.currentPage = 1;
     var createCurPage = function () {
       scope.currentOrderList =
-        $filter('filter')(scope.list, scope.searchText)
+        $filter('filter')(scope.list, scope.searchObj)
         .slice((scope.currentPage - 1) * scope.pageSize);
     };
 
@@ -37,11 +40,50 @@ export default class OrdersController {
     $scope.$watch(angular.bind(this, function () {
       return this.searchText;
     }), function (newVal) {
-      if (!!scope.list) {
+      // $ means apply for all property
+      if (!!scope.list && newVal.length > 0) {
+        scope.searchObj = {
+          $: newVal
+        };
         createCurPage();
       }
     });
+
+    this.rs.$on('filterShipStatus', function (event, request) {
+      scope.searchText = '';
+      scope.searchObj = !!scope.searchObj.ship_status &&
+        scope.searchObj.ship_status === request ? {} : { ship_status: request };
+      createCurPage();
+    });
+
+    this.rs.$on('filterStatus', function (event, request) {
+      scope.searchText = '';
+      scope.searchObj = !!scope.searchObj.status &&
+        scope.searchObj.status === request ? {} : { status: request };
+      createCurPage();
+    });
+  }
+
+  showUnshippedOrders () {
+    this.rs.$broadcast('filterShipStatus', 'notyet');
+  }
+
+  showShippedOrders () {
+    this.rs.$broadcast('filterShipStatus', 'shipped');
+  }
+
+  showSuccessOrders () {
+    this.rs.$broadcast('filterStatus', 'success');
+  }
+
+  showPendingOrders () {
+    this.rs.$broadcast('filterStatus', 'pending');
+  }
+
+  showFailedOrders () {
+    this.rs.$broadcast('filterStatus', 'failed');
   }
 }
 
-OrdersController.$inject = ['$state', '$cookies', 'acl', '$http', 'restful', '$filter', '$scope', '$q'];
+OrdersController.$inject = ['$state', '$cookies', 'acl', '$http', 'restful',
+'$filter', '$scope', '$q', '$rootScope'];
